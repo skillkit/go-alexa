6e373e40aaa2bab872fd45e6fe1890b3f782c9a5
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-12-22 09:11:56 +0100

    Update apex example

--- examples/lambda-apex/main.go
+++ examples/lambda-apex/main.go
@@ -8,21 +8,21 @@ import (
 )
 
 func main() {
+	app := alexa.NewApp(&alexa.Options{
+		ApplicationID: "amzn1.ask.skill.aaa",
+	})
+
+	app.OnIntent(func(w alexa.ResponseWriter, r *alexa.Request) error {
+		w.OutputSpeech("Hello, world!")
+		return nil
+	})
+
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 		var request *alexa.Request
 
 		if err := json.Unmarshal(event, &request); err != nil {
 			return nil, err
 		}
-
-		app := alexa.NewApp(&alexa.Options{
-			ApplicationID: "amzn1.ask.skill.aaa",
-		})
-
-		app.OnIntent(func(w alexa.ResponseWriter, r *alexa.Request) error {
-			w.OutputSpeech("Hello, world!")
-			return nil
-		})
 
 		return app.Process(request)
 	})
